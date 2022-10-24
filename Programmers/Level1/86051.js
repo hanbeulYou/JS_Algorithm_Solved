617ba52071f938c8cd7c8d9fb34700addc5ebeea
hanbeulYou <hanbeul.you@gmail.com>
2022-10-24 14:01:28 +0900

    221024 algorithm solved

--- Programmers/Level1/86051.js
+++ Programmers/Level1/86051.js
@@ -1,5 +1,11 @@
 function solution(numbers) {
-  const arr 
-  var answer = -1;
+  const checkArr = []
+  for(i=0; i<10; i++) checkArr.push(i);
+  numbers.forEach(element =>
+    checkArr[checkArr.indexOf(element)] = 0
+    )
+  const answer = checkArr.reduce((sum, element) => sum+=element, 0)
   return answer;
 }
+
+console.log(solution([1,2,3,4,6,7,8,0]))
