0b1c39e4b58c56664693ee8a2faacdf92f1cbd23
Hanbeul You <109324631+hanbeulYou@users.noreply.github.com>
2022-11-05 23:19:44 +0900

    221105 algorithm solved

---
+++ Leetcode/589. N-ary Tree Preorder Traversal.js
@@ -0,0 +1,19 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+var preorder = function(root, arr = []) {
+  if(!root) return arr;
+  arr.push(root.val)
+  for(let child of root.children) preorder(child, arr)
+
+  return arr;
+};
