109913cddc0b04936919bac36609e75296281561
Hanbeul You <109324631+hanbeulYou@users.noreply.github.com>
2022-12-06 22:16:59 +0900

    221206 algorithm solved

---
+++ Programmers/Level2/12945.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let answer = 0
+    let a = 0
+    let b = 1
+    for(let i=2; i<=n; i++) {
+        answer = (a + b)%1234567
+        a = b%1234567
+        b = answer%1234567
+    }
+    return answer;
+}
