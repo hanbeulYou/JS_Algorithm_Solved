bef8ba38b866188dbfd14c27c82af0f76a0a793e
hanbeulYou <hanbeul.you@gmail.com>
2023-01-13 01:52:51 +0900

    230112 algorithm solved

--- Programmers/Level1/147355.js
+++ Programmers/Level1/147355.js
@@ -1,8 +1,9 @@
 function solution(t, p) {
+  let answer = 0
   const intP = parseInt(p)
-  for(let i=0; i<t.length-p.length; i++) {
+  for(let i=0; i<=t.length-p.length; i++) {
     const newT = parseInt(t.slice(i, i+p.length))
-    console.log(newT)
+    if(newT <= intP) answer += 1
   }
-  return -1;
+  return answer;
 }
---
+++ Programmers/Level1/150370.js
@@ -0,0 +1,28 @@
+const validities = {}
+
+function makeDate(date) {
+  const year = parseInt(date.slice(0, 4))*28*12
+  const month = parseInt(date.slice(5, 7))*28
+  const day = parseInt(date.slice(8, 10))
+  return year + month + day
+}
+
+function makeValidities(terms) {
+  terms.forEach((term) => {
+    [whichTerm, month] = term.split(" ")
+    validities[whichTerm] = parseInt(month) * 28
+  })
+}
+
+function solution(today, terms, privacies) {
+  const todayDate = makeDate(today)
+  const answer = []
+  makeValidities(terms)
+  console.log(todayDate)
+  privacies.forEach((privacy, index) => {
+    const startDate = makeDate(privacy)
+    const validity = privacy.slice(-1)
+    if(startDate + validities[validity] <= todayDate) answer.push(index+1)
+  })
+  return answer
+}
