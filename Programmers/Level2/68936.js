43d702522dfaa070b8f7c5716611c010ca21cd9f
hanbeulYou <hanbeul.you@gmail.com>
2023-04-19 22:34:26 +0900

    230419 algorithm solved

--- Programmers/Level2/68936.js
+++ Programmers/Level2/68936.js
@@ -1,32 +1,23 @@
 const answer = [0, 0]
 
-function check(arr, startI, startJ, endI, endJ) {
-  let tmp = arr[startI-1][startJ-1];
-  let needDivied = false;
-  if(startI === endI && startJ === endJ) {
-    answer[tmp] += 1;
-    return;
-  }
-  for(let i=startI-1; i<endI; i++) {
-    for(let j=startJ-1; j<endJ; j++) {
-      if(tmp !== arr[i][j]) {
-        needDivied = true;
-        break;
+function check(arr, startI, startJ, len) {
+  let tmp = arr[startI][startJ];
+  for(let i=0; i<len; i++) {
+    for(let j=0; j<len; j++) {
+      if(tmp !== arr[startI+i][startJ+j]) {
+        check(arr, startI, startJ, len/2);
+        check(arr, startI, startJ+len/2, len/2);
+        check(arr, startI+len/2, startJ, len/2);
+        check(arr, startI+len/2, startJ+len/2, len/2);
+        return;
       }
     }
-    if(needDivied) break;
-  }
-  if(needDivied) {
-    check(arr, startI, startJ, endI/2, endJ/2);
-    check(arr, endI/2, startJ, endI, endJ/2);
-    check(arr, startI, endJ/2, endI/2, endJ);
-    check(arr, endI/2, endJ/2, endI, endJ);
   }
   answer[tmp] += 1;
 }
 
 function solution(arr) {
-  check(arr, 1, 1, arr.length, arr.length)
+  check(arr, 0, 0, arr.length)
   return answer;
 }
 
